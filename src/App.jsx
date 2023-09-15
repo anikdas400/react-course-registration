71b0361c891bcd1719b1d7f9195a53704c939d2a
anikdas400 <anikchandra400@gmail.com>
2023-09-15 14:37:00 +0600

    conditionaly add totalCredit totalPrice and remain

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,35 @@ import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
   const [bookmarks,setBookmarks]=useState([])
+  const [remaining,setRemaining] =useState(20)
+  const [totalCredit,setTotalCredit]=useState(0)
+  const [totalPrice,setTotalPrice]=useState(0)
 
   const handleAddCourse = course =>{
 
     const isExist = bookmarks.find((item)=>item.id == course.id)
+    let count = course.credit
+    let price =course.price
     if(isExist){
      return alert('this item has been already exist')
     }
     else{
+      bookmarks.forEach((item)=>{
+        count=count +item.credit
+        price=price +item.price
+      })
+
+      
+      const totalremaining = 20-count;
+
+      if(count > 20){
+        return alert("your credit hours has been finished")
+      }
+
+      
+      setTotalPrice(price)
+      setTotalCredit(count)
+      setRemaining(totalremaining)
       const newBookmarks=[...bookmarks,course]
       setBookmarks(newBookmarks)
     }
@@ -32,6 +53,9 @@ function App() {
         <Blogs handleAddCourse={handleAddCourse}></Blogs>
         <Bookmarks 
         bookmarks={bookmarks}
+        remaining={remaining}
+        totalCredit={totalCredit}
+        totalPrice={totalPrice}
         ></Bookmarks>
         
       </div>
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,12 @@
 
 
 
-const Bookmarks = ({bookmarks}) => {
+const Bookmarks = ({bookmarks,remaining,totalCredit,totalPrice}) => {
     // const{id,credit,course_name}=bookmarks
     return (
         <div className="md:w-1/4">
             <h2 className="text-3xl">Bookmarks:{bookmarks.length}</h2>
+            <h3>Credit Hour Remaining:{remaining}</h3>
             <h2 className=" mb-5 text-2xl font-semibold ">Course Name</h2>
             
             {
@@ -13,7 +14,8 @@ const Bookmarks = ({bookmarks}) => {
                     <li key={idx} className=" list-decimal">{bookmark.course_name}</li>
                 ))
             }
-            <h2 className="mt-4">Total Credit Hour :</h2>
+            <h2 className="mt-4">Total Credit Hour : {totalCredit}</h2>
+            <h4>Total Price: {totalPrice} USD</h4>
             
             
         </div>
